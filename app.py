e006a031d6cfb3764e15fba6054cca556f4c276b
AV18 <noronhaavil58@gmail.com>
2026-02-20 11:42:50 +0530

    Updated for deployment

--- app.py
+++ app.py
@@ -409,4 +409,8 @@ if __name__ == '__main__':
     print("="*60 + "\n")
     
     # Run the app
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
